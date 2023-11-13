687e1108ab440265dc9a3a70ace1b8edece00450
mdinalayubi <mdinalayubi@gmail.com>
2023-11-13 13:36:57 +0700

    Upload

---
+++ penggajian.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Form Input</title>
+        <meta charset="utf-8">
+
+        <!-- CSS -->
+        <style>
+        body {
+            width: 100%;
+            height: 100vh;
+            margin: 0;
+            font-family: tahoma;
+            font-size: 16px;
+        }
+        h1, p {
+            margin: 1em auto;
+            text-align: center;
+        }
+        form {
+            width: 60vw;
+            max-width: 500px;
+            min-width: 300px;
+            margin: 0 auto;
+            padding-bottom: 2em;
+            }
+        label {
+            display: block;
+            margin: 0.5rem 0;
+        }
+        button[type="submit"] {
+            display: block;
+            width: 60%;
+            margin: 1em auto;
+            height: 2em;
+            font-size: 1.1rem;
+            background-color: #e0daca;
+            border-color: white;
+            min-width: 300px;
+        }
+        </style>
+    </head>
+
+    <body>
+        <h1 id="title">Survey Formulir Pekerjaan dan Gaji</h1>
+        <p id="description"><i>Melansir dari situs <a href="https://id.indeed.com/career/php-developer/salaries"> indeed.com</a></i></p>
+
+        <form id="survey-form" action="" method="POST">
+            <fieldset>
+                <label>Nama: 
+                    <input type="text" name="nama" required/>
+                </label>
+                <label>Tanggal lahir: 
+                    <input type="date" name="date">
+                </label>
+                <label>Pekerjaan: 
+                    <label>
+                    <input type="radio" name="job" value="0"/>Database Administrator
+                    </label>
+                    <label>
+                    <input type="radio" name="job" value="1"/>Software Developer
+                    </label>
+                    <label>
+                    <input type="radio" name="job" value="2"/>Web Developer
+                    </label>
+                </label>
+                <button type="submit" name="submit">Kirim</button>
+            </fieldset>
+            <fieldset>
+                <?php
+                if( isset($_POST['submit']))
+                {
+                    $nama = $_POST['nama'];
+                    $date = $_POST['date'];
+                    $job = $_POST['job'];
+
+
+                    $date_user = new DateTime($date);
+                    $today =  new DateTime('today');
+                    $usia = $today->diff($date_user)->y;
+
+                    $job_array = ["Database Administrator","Software Developer","Web Developer"];
+                    $salary_array = ["Rp. 5.300.000","Rp. 5.400.000","Rp. 4.800.000"];
+
+
+                    echo "Halo, ".$nama."<br>Kamu lahir pada tanggal ".$date.", Usia mu ".$usia." tahun";
+                    echo "<br>Pekerjaan yang kamu pilih adalah ".$job_array[(int)$job].", dengan penghasilan kurang lebih ".$salary_array[(int)$job];
+                }
+                ?>
+            </fieldset>
+        </form>
+    </body>
+</html>
